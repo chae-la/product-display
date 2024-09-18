403864d2cfb7906a4ab3f70997e50fe34b0d71cc
Michaela <michaelachan45@gmail.com>
2024-09-18 18:02:15 +0100

    parallax for single product

--- src/container/ProductInfo/ProductInfo.tsx
+++ src/container/ProductInfo/ProductInfo.tsx
@@ -7,21 +7,24 @@ type ProductInfoProps = {
 };
 
 const ProductInfo = ({ products }: ProductInfoProps) => {
-    const { productId } = useParams<{ productId: string }>();
+  const { productId } = useParams<{ productId: string }>();
 
-    
-    const product = products.find(p => p.productId === parseInt(productId ?? "", 10));
-  if (product === undefined) return <p>No Products with that ID</p>;
+  const product = products.find(p => p.productId === parseInt(productId ?? "", 10));
+
+  if (!product) return <p>No Products with that ID</p>;
 
   return (
     <div className="product-info">
-      <img src={product.image} className="product-info__image" />
+      <div 
+        className="product-info__image" 
+        style={{ backgroundImage: `url(${product.image})` }} 
+      />
       <div className="product-info__container">
         <h3 className="product-info__container product-info__container--heading">
           {product.productName} - {product.brand}
         </h3>
-        <h3 className="product-info__container product-info__container--heading">
-          {product.rating}
+        <h3 className="product-info__container product-info__container--rating">
+          Rating: {product.rating}/10
         </h3>
         <p className="product-info__container product-info__container--review">Lorem ipsum etcetc...</p>
       </div>
